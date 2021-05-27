83c9c3c2e4a38ec36e374b92c08d20d333193f26
SangjinH <54434944+SangjinH@users.noreply.github.com>
2021-05-27 14:26:20 +0900

    update profile

--- src/views/Accounts/Profile.vue
+++ src/views/Accounts/Profile.vue
@@ -4,40 +4,113 @@
       <div class="col-md-8 mt-3 my-5">
         <div class="card card-widget widget-user">
           <button class="d-flex justify-content-end m-2" style="border: 0; outline: 0; background-color: white;"
-          @click.prevent="editProfile"><i class="fas fa-edit"></i></button>
-          <!-- Add the bg color to the header using any of the bg-* classes -->
-          <div class="widget-user-header" style="background-image:url('@/assets/user-cover.jpg')">
+          data-bs-toggle="modal" data-bs-target="#profilemodal" 
+          ><i class="fas fa-edit"></i></button>
+          <!-- Modal -->
+          <div class="modal fade" id="profilemodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog d-flex justify-content-center">
+              <div class="modal-content p-4 mt-3" style="width: 50vw;">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Edit Profile
+                  </h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                  <!-- image 수정 -->
+                  <h3>Profile Image</h3>
+                  <!-- file upload start -->
+                  <div class="form-group">
+                    <form>
+                      <div class="form-group d-flex flex-column align-items-start">
+                        <label for="exampleFormControlFile1">Update Your Image</label>
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1" @change="handleFileChange"/>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- file upload end -->
+
+                  <hr>
+                  <!-- genre 수정 -->
+                  <h3>Like Genre</h3>
+                  <!-- {{ genres }} -->
+                    <div v-for="genre in genres"
+                    :key="genre.genre_id"                    
+                    class="form-check form-check-inline">
+                      <input class="form-check-input" type="checkbox" id="inlineCheckbox1">
+                      <label class="form-check-label" for="inlineCheckbox1">{{ genre.genre_name }}</label>
+                    </div>
+                    
+                
+                  <hr>
+                  <!-- place 수정 -->
+                  <h3>Like Place</h3>
+                  <div>
+                    <!-- {{ profile.user.like_locations }} -->
+                    <p v-for="location in profile.user.like_locations"
+                    :key="location.id">
+                      {{ location.name }}
+                      <button v-if="location in profile.user.like_locations" class="border-0 bg-transparent" 
+                      @click="clickUnlike(location)">
+                        🤍
+                      </button>
+                      <button v-else class="border-0 bg-transparent" 
+                      @click="clickUnlike(location)">
+                        ❤️
+                      </button>
+                    </p>
+                  </div>  
+                  <!-- <div v-if="profile.user.like_locations" :v-for="location in profile.user.like_locations"
+                  :key="location.id"
+                  > -->
+                  <!-- {{ location.name }} -->
+                  <!-- </div> -->
+                  <!-- <div v-else>
+                    No Like Locations!
+                  </div> -->
+                  <hr>
+                  <!-- comment 수정 -->
+                  <!-- <h3>My Comment</h3>
+                  {{ profile }} -->
+                </div>
+              </div>
+            </div>
           </div>
+          <!-- modal end -->
+          <!-- Add the bg color to the header using any of the bg-* classes -->
+          <!-- <div class="widget-user-header" style="background-image:url('@/assets/user-cover.jpg')">
+          </div> -->
           <div class="widget-user-image">
-            <img class="img-circle my-3" :src="profile.image" alt="User Avatar">
+            <img class="my-3 rounded-circle" style="width: 18rem; height: 18rem;" :src="profile.image" alt="User Avatar">
             <p>{{ profile.user.username }}</p>
             <p><i class="far fa-envelope"></i> {{ profile.user.email}}</p>
           </div>
           <div class="card-footer">
             <div class="row">
-              <div class="col-sm-4 border-right">
+              <div class="col-sm-6 border-right">
                 <div class="description-block">
                   <h5 class="description-header">{{ profile.user.like_genres.length }}</h5>
-                  <span class="description-text">Genre</span>
+                  <span class="description-text">Like Genre</span>
                 </div>
                 <!-- /.description-block -->
               </div>
               <!-- /.col -->
-              <div class="col-sm-4 border-right">
+              <div class="col-sm-6">
                 <div class="description-block">
                   <h5 class="description-header">{{ profile.user.like_locations.length }}</h5>
-                  <span class="description-text">Place</span>
+                  <span class="description-text">Like Place</span>
                 </div>
                 <!-- /.description-block -->
               </div>
               <!-- /.col -->
-              <div class="col-sm-4">
+              <!-- <div class="col-sm-4">
                 <div class="description-block">
                   <h5 class="description-header">{{ profile.user.like_comments.length }}</h5>
-                  <span class="description-text">Comments</span>
-                </div>
+                  <span class="description-text">My Comments</span>
+                </div> -->
                 <!-- /.description-block -->
-              </div>
+              <!-- </div> -->
               <!-- /.col -->
             </div>
             <!-- /.row -->
@@ -57,6 +130,10 @@ export default {
   data: function () {
     return {
       profile: this.getProfile(),
+      genres: null,
+      image: null,
+      like_genres: null
+
     }
   },
   methods: {
@@ -81,21 +158,55 @@ export default {
           console.log(err)
         })
     },
-    editProfile: function () {
+    // editProfile: function () {
+    //   axios({
+    //     method: 'put',
+    //     url: `${SERVER_URL}/accounts/${this.$store.getters.decodedToken.username}/profile/`,
+    //     headers: this.setToken(),
+    //   })
+    //     .then(res => {
+    //       console.log('요청 보낸다!')
+    //       console.log(res)
+    //     })
+    //     .catch(err => {
+    //       console.log(err)
+    //     })
+    // },
+    getGenres: function () {
       axios({
-        method: 'put',
-        url: `${SERVER_URL}/accounts/${this.$store.getters.decodedToken.username}/profile/`,
-        headers: this.setToken(),
+        method: 'get',
+        url: `${SERVER_URL}/movies/genre/`,
+        headers: this.setToken()
       })
-        .then(res => {
-          console.log('요청 보낸다!')
-          console.log(res)
+        .then(res=> {
+          this.genres = res.data
         })
-        .catch(err => {
+        .catch(err=> {
           console.log(err)
         })
-    }
+    },
+    handleFileChange(e) {
+      this.image = e.target.files[0]
+      console.log(this.image)       
+    },
+    clickUnlike(location) {
+      axios({
+        method: 'post',
+        url: `${SERVER_URL}/movies/${location.id}/like/`,
+        headers: this.setToken(),
+      })
+        .then( res =>{
+          console.log(res)
+          this.getProfile()
+        })
+        .catch( err =>{
+          console.log(err)
+        })
+    },
   },
+  created: function () {
+    this.getGenres()
+  }
 }
 </script>
 
